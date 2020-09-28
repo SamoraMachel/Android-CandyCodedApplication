60b313e517bd0d18be51ecad0ec0ccaef8c1d480
Samora Machel <samoraok@gmail.com>
2020-09-28 10:36:15 +0300

    Hard coded the google map package

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -31,7 +31,8 @@ public class InfoActivity extends AppCompatActivity {
     public void createMapIntent(View view){
         Uri uriAddress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriAddress );
-        mapIntent.setPackage(getString(R.string.googleMaps));
+        mapIntent.setPackage("com.google.android.apps.maps");
+//        getString(R.string.googleMaps) -- passed inside the mapIntent.setPackage()
         if (mapIntent.resolveActivity(getPackageManager()) != null){
             startActivity(mapIntent);
         }
